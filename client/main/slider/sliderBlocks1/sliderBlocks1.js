97ea83f6dd461794f16337d95a39b7f998c4913f
bazubrick <seregi123@mail.ru>
2015-11-11 17:18:47 +0200

    fix nav

--- client/main/slider/sliderBlocks1/sliderBlocks1.js
+++ client/main/slider/sliderBlocks1/sliderBlocks1.js
@@ -1,4 +1,17 @@
 $( document ).ready(function() {
+
+
+});
+/*Template.slider.onCreated(function() {
+    console.log('111');
+
+
+
+});*/
+Template.sliderBlock1.onRendered(function() {
     $('.rightPopUp').hide();
-        $('.rightPopUp').fadeIn(2000);
+    $('.rightPopUp').fadeIn(2000);
+});
+Template.main.onRendered(function() {
+    $('.rightPopUp').hide();
 });
